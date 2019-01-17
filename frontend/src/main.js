050bc0f55eb261a848e759316ed9f3cebddd970f
unknown <422705223@qq.com>
2019-01-17 15:15:21 +0800

    update to vue-cli 3.0

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,9 +16,7 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
