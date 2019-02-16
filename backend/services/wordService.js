88ae18c527dc777f698077f83e7b86eeb6971f7e
unknown <422705223@qq.com>
2019-02-16 16:18:16 +0800

    add word api

--- backend/services/wordService.js
+++ backend/services/wordService.js
@@ -14,15 +14,15 @@ const getWords = async () => {
 const updateWord = async (word) => {
   let sql = `update WORD set TITLE=?, DESCRIPTION=?, CONTENT=? where ID=?`
   let params = [word.title, word.description, word.content, word.id]
-  let data = await query(sql, params)
-  return data
+  await query(sql, params)
+  return
 }
 
 const addWord = async (word) => {
   let sql = 'insert into WORD(TITLE, DESCRIPTION, CONTENT) values (?,?,?)'
   let params = [word.title, word.description, word.content]
-  let data = await query(sql, params)
-  return data
+  await query(sql, params)
+  return
 }
 
 module.exports = {
